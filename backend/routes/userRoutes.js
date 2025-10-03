a6d8941713c9908705ee63567d6fb4f552dcb282
Victor Munhoz <130310222+Vitinhomunhoz@users.noreply.github.com>
2025-10-03 10:59:58 -0300

    Completa o CRUD de usuários com as rotas de Update e Delete

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,21 @@
-const express = require("express");
-const router = express.Router();  authController = require("../controllers/auth"); 
-router.post("/register", authController.register);
-router.post("/login", authController.login);
+const express = require('express');
+const router = express.Router();
+
+const {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+} = require('../controllers/userController');
+
+router.route('/')
+  .get(getAllUsers)   
+  .post(createUser);  
+
+router.route('/:id')
+  .get(getUserById)   
+  .put(updateUser)   
+  .delete(deleteUser); 
+
 module.exports = router;
